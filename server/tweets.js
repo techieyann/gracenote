1c6602f7748989b62ab2f6c81d0c8b26200100b1
Ian McEachern <techieyann@gmail.com>
2015-03-09 15:11:47 -0700

    added location double checking and 'tweet' type

--- server/tweets.js
+++ server/tweets.js
@@ -20,17 +20,24 @@ var streamSync = Meteor.wrapAsync(stream.on, stream);
 streamSync('tweet', function(tweet) {
 	//only add tweets that have coordinates and while the users desire it
 	if (tweet.coordinates && Tweets.findOne('settings').running) {
-		var options = {
-			text: tweet.text,
-			user: tweet.user.screen_name,
-			tweet_id: tweet.id_str,
-			coordinates: tweet.coordinates,
-			heading: 0,
-			readable_date: tweet.created_at,
-			created_at: tweet.timestamp_ms
-		};
-		Tweets.insert(options);
-		Meteor.call('limitTweets');
+		//check if tweet coordinates are actually within specified range
+		var lon = tweet.coordinates.coordinates[0];
+		var lat = tweet.coordinates.coordinates[1];
+		if ((SF[0]<lon && SF[2]>lon) && (SF[1]<lat && SF[3]>lat)) {
+			//strip out the useful data
+			var options = {
+				type: 'tweet',
+				text: tweet.text,
+				user: tweet.user.screen_name,
+				tweet_id: tweet.id_str,
+				coordinates: tweet.coordinates,
+				heading: 0,
+				readable_date: tweet.created_at,
+				created_at: tweet.timestamp_ms
+			};
+			Tweets.insert(options);
+			Meteor.call('limitTweets');
+		}
 	}
 });
 
